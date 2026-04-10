5f284c86d7a1ae47a47c2ea6b3a1ac4a305ad9a3
悠游 <wy512553@taobao.com>
2026-04-10 21:41:16 +0800

    commit scripts

---
+++ run_clm.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+## pretrain code for tinyllama
+#  - to use the minipile dataset, use `--dataset_name JeanKaddour/minipile`, with proper `--preprocessing_num_workers`
+#  - to enable wandb, use `--report_to wandb`
+accelerate launch run_clm.py \
+    --tokenizer_name TinyLlama/TinyLlama-1.1B-intermediate-step-955k-token-2T \
+    --config_name configs/tinyllama_yoco_plus.json \
+    --dataset_name wikitext \
+    --dataset_config_name wikitext-103-raw-v1 \
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 32 \
+    --auto_find_batch_size \
+    --gradient_accumulation_steps 1 \
+    --block_size 1024 \
+    --lr_scheduler_type cosine \
+    --warmup_ratio 0.015 \
+    --learning_rate 3e-4 \
+    --weight_decay 1e-1 \
+    --bf16 \
+    --torch_dtype bfloat16 \
+    --do_train \
+    --do_eval \
+    --use_liger_kernel \
+    --num_train_epochs 3 \
+    --save_total_limit 1 \
+    --save_strategy steps \
+    --save_steps 500 \
+    --evaluation_strategy steps \
+    --eval_steps 500 \
+    --load_best_model_at_end True \
+    --metric_for_best_model eval_loss \
+    --report_to none \
+    --run_name tinyllama-test \
+    --overwrite_output_dir \
+    --output_dir outputs/tinyllama-test
---
+++ run_generation.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# run generation
+python run_generation.py \
+    --model_type lckv-llama \
+    --torch_dtype bfloat16 \
+    --tokenizer_name TinyLlama/TinyLlama-1.1B-intermediate-step-955k-token-2T \
+    --model_name_or_path outputs/tinyllama-test \
+    --num_return_sequences 1 \
+    --prompt "the meaning of life is" \
+    --length 512
+
+# run streaming
+python run_generation.py \
+    --model_type lckv-llama \
+    --torch_dtype bfloat16 \
+    --tokenizer_name TinyLlama/TinyLlama-1.1B-intermediate-step-955k-token-2T \
+    --model_name_or_path outputs/tinyllama-test \
+    --num_return_sequences 1 \
+    --prompt "the meaning of life is" \
+    --length 2048 \
+    --sink_cache \
+    --window_length 1024 \
+    --num_sink_tokens 4 \
